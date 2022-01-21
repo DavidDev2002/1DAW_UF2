53060124309d86d723ab4f9351ce9a69abf043f9
DavidDev2002 <95426102+DavidDev2002@users.noreply.github.com>
2022-01-21 16:47:18 +0100

    Exercici_8

---
+++ Exercici_8/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "functions.h"
+
+void main() {
+	int x,y,p;
+	x=validar_nat("Introduce la base de la potencia:  ");
+	y=validar_nat("Introduce el exponente de la potencia:  ");
+	p=potencia(x,y);
+	printf("El resultado de la potencia es: [%d]",p);
+}
